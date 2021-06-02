3c1d5f84269d84385a6cf6c4b74add58ff9f001c
reimibeta <pinsoneatra.soneatra846@gmail.com>
2021-06-02 17:50:54 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ import setuptools
 
 setuptools.setup(
     name='staff_management_models',
-    version='1.0.3',
+    version='1.0.4',
     packages=setuptools.find_packages(),
     install_requires=[
-        'staff-models @ git+https://github.com/reimibeta/django-staff-models.git',
+        'staff-models @ git+https://github.com/reimibeta/django-staff-models.git@4a245b6b9804dd897e98ca3af748ee7f8aae294d',
     ]
     # scripts=['manage.py']
 )
